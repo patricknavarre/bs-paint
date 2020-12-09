9819f29e7ea08e62c1e075a0744c2ac1771228da
patricknavarre <patrick.navarre@codeimmersives.com>
2020-12-09 12:00:46 -0500

    completed-minus-mousedown

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 /*******************
  * OUR HELPER CODE *
 *******************/
-
 /*
  * Here we add the squares to the canvas dynamically.
  * You can mostly leave this section alone!
@@ -15,7 +14,7 @@
  * To make the second one happen, the number to change
  * is the first argument to `repeat`, currently set at 10.
  */
-const gridWidth = 10;
+const gridWidth = 50;
 let count = 0;
 while (count <= gridWidth * gridWidth) {
   const canvas = document.querySelector('.canvas');
@@ -25,35 +24,62 @@ while (count <= gridWidth * gridWidth) {
   count++;
 }
 
-// You probably should NOT do these in the order below.
-// That is, you probably should NOT do all the queries,
-// THEN all the functions,
-// THEN all the wiring.
-
-// Instead, it'll be easier if you go one action at a time!
-// So, add a query for the palette colors.
-// THEN add an event listener function for what happens when one is clicked.
-// THEN wire those two together, so that when the palette elements are clicked,
-// the function runs.
-//
-// And proceed from there to getting the squares working.
-//
-
-// ALSO.
-// You do not have to follow the sections below. If you're doing your functions inline, it doesn't make a lot of sense to separate the event listener functions from their wiring!
-
 /***********
  * QUERIES *
 ***********/
-
 // Add queries for all your squares, palette colors, and brush here.
 // (Note the singular or plural used in that sentence!)
+const brush = document.querySelector('.current-brush').classList
+const canvas = document.querySelectorAll('.square')
 
+const color1 = document.querySelector('.color-1')
+const color2 = document.querySelector('.color-2')
+const color3 = document.querySelector('.color-3')
+const color4 = document.querySelector('.color-4')
+const color5 = document.querySelector('.color-5')
+const color6 = document.querySelector('.color-6')
+const color7 = document.querySelector('.color-7')
+const color8 = document.querySelector('.color-8')
+const color9 = document.querySelector('.color-9')
+const color10 = document.querySelector('.color-10')
 
+const eraseButton = document.querySelector('.erase')
 
 /****************************
  * EVENT LISTENER FUNCTIONS *
 ****************************/
+
+const changeToColor1 = () => {
+  brush.replace(`${brush[1]}`,'color-1')
+}
+const changeToColor2 = () => {
+  brush.replace(`${brush[1]}`,'color-2')
+}
+const changeToColor3 = () => {
+  brush.replace(`${brush[1]}`,'color-3')
+}
+const changeToColor4 = () => {
+  brush.replace(`${brush[1]}`,'color-4')
+}
+const changeToColor5 = () => {
+  brush.replace(`${brush[1]}`,'color-5')
+}
+const changeToColor6 = () => {
+  brush.replace(`${brush[1]}`,'color-6')
+}
+const changeToColor7 = () => {
+  brush.replace(`${brush[1]}`,'color-7')
+}
+const changeToColor8 = () => {
+  brush.replace(`${brush[1]}`,'color-8')
+}
+const changeToColor9 = () => {
+  brush.replace(`${brush[1]}`,'color-9')
+}
+const changeToColor10 = () => {
+  brush.replace(`${brush[1]}`,'color-10')
+}
+
 
 // Now add some functions to handle clicking one particular square
 // and clicking one particular palette color. You can leave them
@@ -61,13 +87,41 @@ while (count <= gridWidth * gridWidth) {
 // run as event listeners (after the next step is set up) isn't a
 // bad idea for testing purposes.
 
+const clickedPixel = (event) => { 
+  let squareClicked = event.target;
+  squareClicked.classList.replace(squareClicked.classList[1],`${brush[1]}`)
+}
 
+for (const pixel of canvas) {
+  pixel.addEventListener('click', clickedPixel)
+}
+
+
+const eraseAll = () => { 
+  for(const pixel of canvas) {
+    pixel.classList.replace(pixel.classList[1], 'color-5')
+  }
+}
 
 /**************************
  * WIRING IT ALL TOGETHER *
-**************************/
-
+ **************************/
 // Now: wiring up our event listeners to our html node elements.
+
 // You'll need to add the appropriate event listener for each
 // square and for each palette color from the functions you
 // wrote above.
+color1.addEventListener('click', changeToColor1)
+color2.addEventListener('click', changeToColor2)
+color3.addEventListener('click', changeToColor3)
+color4.addEventListener('click', changeToColor4)
+color5.addEventListener('click', changeToColor5)
+color6.addEventListener('click', changeToColor6)
+color7.addEventListener('click', changeToColor7)
+color8.addEventListener('click', changeToColor8)
+color9.addEventListener('click', changeToColor9)
+color10.addEventListener('click', changeToColor10)
+eraseButton.addEventListener('click', eraseAll)
+
+
+
