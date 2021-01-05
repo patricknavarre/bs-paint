ae73abb9bfc647e903273665e4db154e7a8196f8
patricknavarre <patrick.navarre@codeimmersives.com>
2021-01-05 01:01:38 -0500

    restyled-added-media-query

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-/*******************
- * OUR HELPER CODE *
- *******************/
-/*
- * Here we add the squares to the canvas dynamically.
- * You can mostly leave this section alone!
- * But if you want to change how wide the canvas is,
- * there are just two steps:
- *
- * 1. Change the `gridWidth` value below.
- * 2. Change the `grid-template-rows` and
- * `grid-template-columns` to match.
- *
- * To make the second one happen, the number to change
- * is the first argument to `repeat`, currently set at 10.
- */
 
 const gridWidth = 100;
 let count = 0;
